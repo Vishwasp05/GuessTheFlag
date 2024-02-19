5bef30ec7e14a4ab346cc581f0a214018eaa2992
Vishwas Sharma <ch0nky@protonmail.com>
2024-02-19 18:52:01 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Vishwas Sharma on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
